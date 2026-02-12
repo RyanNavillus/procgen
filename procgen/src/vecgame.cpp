d41a626f115b6c685927e55e667cfcc54e621b3d
RyanNavillus <ryanpnavillus@gmail.com>
2026-02-11 17:25:38 -0800

    Add heist info

--- procgen/src/vecgame.cpp
+++ procgen/src/vecgame.cpp
@@ -283,6 +283,28 @@ VecGame::VecGame(int _nenvs, VecOptions opts) {
         info_types.push_back(s);
     }
 
+    {
+        struct libenv_tensortype s;
+        strcpy(s.name, "heist_key_count");
+        s.scalar_type = LIBENV_SCALAR_TYPE_DISCRETE;
+        s.dtype = LIBENV_DTYPE_INT32;
+        s.ndim = 0,
+        s.low.int32 = 0;
+        s.high.int32 = INT32_MAX;
+        info_types.push_back(s);
+    }
+
+    {
+        struct libenv_tensortype s;
+        strcpy(s.name, "heist_key_total");
+        s.scalar_type = LIBENV_SCALAR_TYPE_DISCRETE;
+        s.dtype = LIBENV_DTYPE_INT32;
+        s.ndim = 0,
+        s.low.int32 = 0;
+        s.high.int32 = INT32_MAX;
+        info_types.push_back(s);
+    }
+
     int level_seed_low = 0;
     int level_seed_high = 0;
 
